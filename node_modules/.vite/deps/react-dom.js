cc84a87dd8af96f4e801e235f291826958033be5
Aminebouramtane <bouramtaneamine1@gmail.com>
2026-04-04 23:59:43 +0100

    feat : generate portfolio just with html and css files

--- node_modules/.vite/deps/react-dom.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-BCXODTBQ.js";
-import "./chunk-2YIMICFJ.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
--- node_modules/.vite/deps/react.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react
-} from "./chunk-2YIMICFJ.js";
-export default require_react();
-//# sourceMappingURL=react.js.map
